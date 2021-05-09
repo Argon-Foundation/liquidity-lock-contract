706722d852e8ef7e824c3089fa9e2ff822a06ff6
İbrahim Keçici <ibrahimkecici4116@gmail.com>
2021-05-09 17:11:17 +0300

    SafeMath and ReentrancyGuard

--- lock.sol
+++ lock.sol
@@ -31,7 +31,73 @@ interface IERC20 {
     );
 }
 
-contract lock {
+contract ReentrancyGuard {
+    // Booleans are more expensive than uint256 or any type that takes up a full
+    // word because each write operation emits an extra SLOAD to first read the
+    // slot's contents, replace the bits taken up by the boolean, and then write
+    // back. This is the compiler's defense against contract upgrades and
+    // pointer aliasing, and it cannot be disabled.
+
+    // The values being non-zero value makes deployment a bit more expensive,
+    // but in exchange the refund on every call to nonReentrant will be lower in
+    // amount. Since refunds are capped to a percentage of the total
+    // transaction's gas, it is best to keep them low in cases like this one, to
+    // increase the likelihood of the full refund coming into effect.
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+
+    uint256 private _status;
+
+    constructor () public {
+        _status = _NOT_ENTERED;
+    }
+
+    /**
+     * @dev Prevents a contract from calling itself, directly or indirectly.
+     * Calling a `nonReentrant` function from another `nonReentrant`
+     * function is not supported. It is possible to prevent this from happening
+     * by making the `nonReentrant` function external, and make it call a
+     * `private` function that does the actual work.
+     */
+    modifier nonReentrant() {
+        // On the first call to nonReentrant, _notEntered will be true
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+
+        // Any calls to nonReentrant after this point will fail
+        _status = _ENTERED;
+
+        _;
+
+        // By storing the original value once again, a refund is triggered (see
+        // https://eips.ethereum.org/EIPS/eip-2200)
+        _status = _NOT_ENTERED;
+    }
+}
+
+library SafeMath {
+    function safeAdd(uint256 a, uint256 b) public pure returns (uint256 c) {
+        c = a + b;
+        require(c >= a);
+    }
+
+    function safeSub(uint256 a, uint256 b) public pure returns (uint256 c) {
+        require(b <= a);
+        c = a - b;
+    }
+
+    function safeMul(uint256 a, uint256 b) public pure returns (uint256 c) {
+        c = a * b;
+        require(a == 0 || c / a == b);
+    }
+
+    function safeDiv(uint256 a, uint256 b) public pure returns (uint256 c) {
+        require(b > 0);
+        c = a / b;
+    }
+}
+
+contract lock is ReentrancyGuard {
+    using SafeMath for uint256;
     struct lockInfo {
         uint256 startedDate;
         uint256 endDate;
@@ -52,8 +118,8 @@ contract lock {
         uint256 _endDate,
         uint256 _amount,
         address _tokenAddress
-    ) public {
-        require(now < _endDate, "endDate should be smaller than now");
+    ) public nonReentrant{
+        require(now < _endDate, "endDate should be bigger than now");
         require(_amount != 0, "amount cannot 0");
         require(
             _tokenAddress != address(0),
@@ -69,7 +135,7 @@ contract lock {
         );
         require(poolCount == 0, "Pool count must be 0");
         pool = lockInfo(now, _endDate, _amount, _tokenAddress, msg.sender);
-        poolCount++;
+        poolCount = poolCount.safeAdd(1);
     }
 
     function getPoolData()
@@ -92,7 +158,7 @@ contract lock {
         );
     }
 
-    function getTokens() public onlyManager {
+    function getTokens() public onlyManager nonReentrant {
         require(now > pool.endDate);
         IERC20(pool.tokenAddress).transfer(msg.sender, pool.amount);
     }
